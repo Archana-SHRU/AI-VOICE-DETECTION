b7eb300f866dcc69882c7f74607e5e5f28000972
Archana Shruti <archushru0809@gmail.com>
2026-02-25 10:59:48 +0530

    Delete app directory

--- app/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-API_KEY = os.getenv("API_KEY")
-
-if not API_KEY:
-    raise RuntimeError("API_KEY is not set in environment variables")
